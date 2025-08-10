f2c66a60e2eba4514ede516f02c26237d935fc6a
Jack <gkxkzp2558@naver.com>
2025-08-11 08:17:49 +0900

    step3 push

---
+++ src/main/java/com/study/rabbitmq/step3/notification/NotificationMessage.java
@@ -0,0 +1,11 @@
+package com.study.rabbitmq.step3.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class NotificationMessage {
+    private String message;
+}
